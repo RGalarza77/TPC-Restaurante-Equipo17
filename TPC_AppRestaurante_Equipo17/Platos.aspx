4883c5aa69fdf0b373a327d1b346614547fa9f48
GCuelloARG <gonza.cuello@gmail.com>
2024-06-07 00:31:33 -0300

    Administrar Platos

---
+++ TPC_AppRestaurante_Equipo17/Platos.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="Platos.aspx.cs" Inherits="TPC_AppRestaurante_Equipo17.Platos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-4">
+            <div class="mb-3">
+                <label for="txtCodProd" class="form-label">Codigo Producto</label>
+                <asp:TextBox runat="server" ID="txtCod" CssClass="form-control" />
+            </div>
+            <div class="mb-3">
+                <label for="txtNombre" class="form-label">Nombre</label>
+                <asp:TextBox runat="server" ID="txtNombre" CssClass="form-control" />
+            </div>
+            <div class="mb-3">
+                <label for="txtDescripcion" class="form-label">Descripcion</label>
+                <asp:TextBox runat="server" TextMode="MultiLine" ID="txtDescripcion" CssClass="form-control" />
+            </div>
+            
+            <div class="mb-3">
+                <label for="txtPrecio" class="form-label">Precio</label>
+                <asp:TextBox runat="server" TextMode="Number" ID="txtPrecio" CssClass="form-control" />
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="mb-3">
+                <img src="https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=" class="img-thumbnail" alt="400">
+                <div class="row">
+
+
+
+                </div>
+                
+            </div>
+        </div>
+        <div class="col-4">
+                    <div class="col-6"></div>
+                <div class="col-4">
+                    <div class="mb-4">
+                        <br />
+                        <asp:Button Text="Agregar Imagen" ID="btnAgregarImagen" cssclass="btn btn-primary" runat="server" />
+                    </div>
+                    <div class="mb-4">
+                    </div>
+                    <asp:Button Text="Ingredientes" ID="btnIngredientes" cssclass="btn btn-primary" runat="server" />
+                </div>
+
+
+        </div>
+    </div>
+</asp:Content>
---
+++ TPC_AppRestaurante_Equipo17/Platos.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_AppRestaurante_Equipo17
+{
+
+
+    public partial class Platos
+    {
+
+        /// <summary>
+        /// Control txtCod.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCod;
+
+        /// <summary>
+        /// Control txtNombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombre;
+
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
+
+        /// <summary>
+        /// Control txtPrecio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPrecio;
+
+        /// <summary>
+        /// Control btnAgregarImagen.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAgregarImagen;
+
+        /// <summary>
+        /// Control btnIngredientes.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIngredientes;
+    }
+}
