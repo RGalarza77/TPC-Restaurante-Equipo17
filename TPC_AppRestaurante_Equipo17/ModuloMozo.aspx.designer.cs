9ed879dbcffa884fefad17320b682029a01fa1cb
rgalarza <nahuel_n.n@hotmail.com>
2024-06-07 02:57:28 -0300

    Ventana Modulo mesas

---
+++ TPC_AppRestaurante_Equipo17/ModuloMozo.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_AppRestaurante_Equipo17
+{
+
+
+    public partial class ModuloMozo
+    {
+    }
+}
